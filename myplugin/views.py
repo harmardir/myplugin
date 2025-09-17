72b3789e6d914c92c244da91540402f6b920f1f9
Harout Mardirossian <harmardir@gmail.com>
2025-09-18 01:28:37 +0300

    adding template view

--- myplugin/views.py
+++ myplugin/views.py
@@ -1,4 +1,12 @@
+'''
 from django.http import JsonResponse
 
 def hello_world(request):
     return JsonResponse({"message": "Hello from MyPlugin!"})
+'''
+
+from django.shortcuts import render
+
+def hello_world(request):
+    context = {"message": "Hello from MyPlugin (HTML)!"}
+    return render(request, "myplugin/hello.html", context)
