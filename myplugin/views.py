4531ce33c2688e301954c19be3f78a1202719aa7
Harout Mardirossian <harmardir@gmail.com>
2025-09-18 22:44:32 +0300

    adding units

--- myplugin/views.py
+++ myplugin/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 from rest_framework.test import APIRequestFactory
 from lms.djangoapps.courseware_api.views import CourseViewSet, BlocksViewSet
 
 
 def courses_and_units_view(request):
-    """
-    Fetch all courses and their units using the Courseware API,
-    then render with a Mako template.
-    """
     factory = APIRequestFactory()
 
-    # Call Course API to get all enrolled courses
+    # Call Course API
     course_api_req = factory.get("/api/courseware/course/")
     course_response = CourseViewSet.as_view({"get": "list"})(course_api_req)
 
@@ -19,13 +14,12 @@ def courses_and_units_view(request):
     for course in course_response.data.get("results", []):
         course_id = course["id"]
 
-        # Call Blocks API to get blocks for this course
+        # Call Blocks API
         blocks_req = factory.get(f"/api/courseware/blocks/?course_id={course_id}&all_blocks=true")
         blocks_response = BlocksViewSet.as_view({"get": "list"})(blocks_req)
 
         blocks_data = blocks_response.data.get("blocks", {})
 
-        # Filter units (sequences typically)
         units = [
             {
                 "id": blk_id,
@@ -43,9 +37,4 @@ def courses_and_units_view(request):
         })
 
     context = {"courses": courses}
-
-    return render_to_response(
-        "myplugin/courses_and_units.html",
-        context,
-        context_instance=RequestContext(request),
-    )
+    return render(request, "myplugin/courses_and_units.html", context)
